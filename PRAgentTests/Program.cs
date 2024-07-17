83b1816321ec8139bffcf78877252a5d137e0529
noeltupas <manuel.tupas@gmail.com>
2024-07-16 20:54:15 -0400

    Moved output responsibility to separate dependency. LOL

--- PRAgentTests/Program.cs
+++ PRAgentTests/Program.cs
@@ -1,22 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string test = """
-	In Project Mayhem, we have no name.
-	But in death, we do.
+using PRAgentTests;
 
-	His name, is Robert Paulson.
+new TestDependency("""
+		In Project Mayhem, we have no name.
+		But in death, we do.
 
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	His name is Robert Paulson.
-	""";
+		His name, is Robert Paulson.
 
-Console.WriteLine(test);
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		His name is Robert Paulson.
+		""").WriteThisString();
