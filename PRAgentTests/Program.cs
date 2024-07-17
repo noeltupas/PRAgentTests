7c6e7f8a6e2cfe3988d5607306ba6b09d6175be9
noeltupas <manuel.tupas@gmail.com>
2024-07-16 20:37:01 -0400

    Pushing new code!!

---
+++ PRAgentTests/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string test = """
+	In Project Mayhem, we have no name.
+	But in death, we do.
+
+	His name, is Robert Paulson.
+
+	His name is Robert Paulson.
+	His name is Robert Paulson.
+	His name is Robert Paulson.
+	His name is Robert Paulson.
+	His name is Robert Paulson.
+	His name is Robert Paulson.
+	""";
+
+Console.WriteLine(test);
